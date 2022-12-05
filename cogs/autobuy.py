17f33d38c04815c2cb1477558e07700eb7721acf
BridgeSense <bridgesensemc@gmail.com>
2022-12-05 14:44:03 +0800

    fix: change /buy to /shop buy

--- cogs/autobuy.py
+++ cogs/autobuy.py
@@ -2,6 +2,7 @@ import re
 
 from discord.ext import commands
 
+
 class Autobuy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -18,9 +19,9 @@ class Autobuy(commands.Cog):
                 try:
                     if (
                         embed.to_dict()["title"] == "Your lifesaver protected you!"
-                        and self.bot.config_dict[self.bot.account_id]["autobuy"]["lifesavers"][
-                            "state"
-                        ]
+                        and self.bot.config_dict[self.bot.account_id]["autobuy"][
+                            "lifesavers"
+                        ]["state"]
                         is True
                     ):
                         remaining = int(
@@ -30,23 +31,21 @@ class Autobuy(commands.Cog):
                             ).group(1)
                         )
                         required = int(
-                            self.bot.config_dict[self.bot.account_id]["autobuy"]["lifesavers"][
-                                "amount"
-                            ]
+                            self.bot.config_dict[self.bot.account_id]["autobuy"][
+                                "lifesavers"
+                            ]["amount"]
                         )
                         if remaining < required:
                             channel = await message.author.create_dm()
                             await self.bot.send(
                                 "withdraw", amount=str((required - remaining) * 85000)
                             )
-                            async for cmd in channel.slash_commands(
-                                query="buy", limit=None
-                            ):
-                                await cmd(
-                                    item="Life Saver",
-                                    quantity=str(required - remaining),
-                                )
-                                break
+                            await self.bot.sub_send(
+                                "shop",
+                                "buy",
+                                item="Life Saver",
+                                quantity=str(required - remaining),
+                            )
                             return
                 except KeyError:
                     pass
@@ -55,9 +54,9 @@ class Autobuy(commands.Cog):
                 try:
                     if (
                         embed.to_dict()["title"] == "Pending Confirmation"
-                        and self.bot.config_dict[self.bot.account_id]["autobuy"]["lifesavers"][
-                            "state"
-                        ]
+                        and self.bot.config_dict[self.bot.account_id]["autobuy"][
+                            "lifesavers"
+                        ]["state"]
                         is True
                     ):
                         await self.bot.click(message, 0, 1)
@@ -78,7 +77,7 @@ class Autobuy(commands.Cog):
                     in embed.to_dict()["description"]
                 ):
                     await self.bot.send("withdraw", amount="25k")
-                    await self.bot.send("buy", item="Shovel", quantity="1")
+                    await self.bot.sub_send("shop", "buy", item="Shovel", quantity="1")
             except KeyError:
                 pass
 
@@ -89,7 +88,9 @@ class Autobuy(commands.Cog):
                     in embed.to_dict()["description"]
                 ):
                     await self.bot.send("withdraw", amount="25k")
-                    await self.bot.send("buy", item="Fishing Pole", quantity="1")
+                    await self.bot.sub_send(
+                        "shop", "buy", item="Fishing Pole", quantity="1"
+                    )
             except KeyError:
                 pass
 
@@ -100,7 +101,9 @@ class Autobuy(commands.Cog):
                     in embed.to_dict()["description"]
                 ):
                     await self.bot.send("withdraw", amount="25k")
-                    await self.bot.send("buy", item="Hunting Rifle", quantity="1")
+                    await self.bot.sub_send(
+                        "shop", "buy", item="Hunting Rifle", quantity="1"
+                    )
             except KeyError:
                 pass
 
