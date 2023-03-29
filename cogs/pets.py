6d3d96c7d99036093be0b25714335a18a4d1b11c
BridgeSense <bridgesensemc@gmail.com>
2023-03-29 14:25:41 +0800

    feat: auto pet for all pets

--- cogs/pets.py
+++ cogs/pets.py
@@ -24,28 +24,32 @@ class Pets(commands.Cog):
         for embed in message.embeds:
             embed = embed.to_dict()
             try:
+                embed = message.embeds[0].to_dict()
                 if embed["fields"][0]["name"] == "Hunger":
-                    for count, i in enumerate(embed["fields"]):
-                        percentage = int(
-                            re.search(
-                                "\((.*?)\)",
-                                i["value"],
-                            ).group(
-                                1
-                            )[:-1]
-                        )
-                        while percentage <= 90:
-                            await self.bot.click(message, 1, count)
-                            if count == 2:
-                                return
-                            await asyncio.sleep(0.5)
-                            embed = message.embeds[0].to_dict()
+                    for pet in message.components[0].children[0].options:
+                        await message.components[0].children[0].choose(pet)
+                        await asyncio.sleep(1)
+                        for count, i in enumerate(embed["fields"]):
                             percentage = int(
                                 re.search(
                                     "\((.*?)\)",
-                                    embed["fields"][count]["value"],
-                                ).group(1)[:-1]
+                                    i["value"],
+                                ).group(
+                                    1
+                                )[:-1]
                             )
+                            while percentage <= 90:
+                                await self.bot.click(message, 1, count)
+                                if count == 2:
+                                    break
+                                await asyncio.sleep(0.5)
+                                embed = message.embeds[0].to_dict()
+                                percentage = int(
+                                    re.search(
+                                        "\((.*?)\)",
+                                        embed["fields"][count]["value"],
+                                    ).group(1)[:-1]
+                                )
             except KeyError:
                 pass
 
