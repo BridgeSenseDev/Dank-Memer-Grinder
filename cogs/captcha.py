0bf47a660fc5530979318765f58bf93c422d23cb
BridgeSense <bridgesensemc@gmail.com>
2023-03-29 14:24:23 +0800

    fix: broken captcha solver

--- cogs/captcha.py
+++ cogs/captcha.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from discord.ext import commands
 
 
@@ -18,27 +20,33 @@ class Captcha(commands.Cog):
 
                 # Matching image captcha
                 if "matching image" in embed["description"]:
-                    captcha = embed.image.url
-                    for count, i in enumerate(message.components):
-                        if i.emoji.url in captcha:
+                    captcha_url = embed["image"]["url"]
+                    for count, button in enumerate(message.components[0].children):
+                        if button.emoji.url in captcha_url:
+                            print(f"clicking {count}")
+                            await asyncio.sleep(0.5)
                             await self.bot.click(message, 0, count)
-                            break
+                            return
 
                 # Pepe captcha
                 if "pepe" in embed["description"]:
-                    for count, i in enumerate(message.components):
-                        if i.emoji.id in [
-                            819014822867894304,
-                            796765883120353280,
-                            860602697942040596,
-                            860602923665588284,
-                            860603013063507998,
-                            936007340736536626,
-                            933194488241864704,
-                            680105017532743700,
-                        ]:
-                            await self.bot.click(message, 0, count)
-                            break
+                    for row, i in enumerate(message.components):
+                        for column, button in enumerate(i.children):
+                            if not hasattr(button, "id"):
+                                continue
+                            if button.emoji.id in [
+                                819014822867894304,
+                                796765883120353280,
+                                860602697942040596,
+                                860602923665588284,
+                                860603013063507998,
+                                936007340736536626,
+                                933194488241864704,
+                                680105017532743700,
+                            ]:
+                                await asyncio.sleep(0.3)
+                                await self.bot.click(message, row, column)
+                                continue
 
             except KeyError:
                 pass
