3b08fa027d8ebe4dd0aa9ace312d70e66da47daa
BridgeSense <bridgesensemc@gmail.com>
2023-04-04 14:33:22 +0800

    fix: autobuy items not working because of return

--- cogs/autobuy.py
+++ cogs/autobuy.py
@@ -9,52 +9,51 @@ class Autobuy(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.guild or not self.bot.state:
-            return
-        for embed in message.embeds:
-            embed = embed.to_dict()
-            # Buy lifesavers
-            try:
-                if (
-                    embed["title"] == "Your lifesaver protected you!"
-                    and self.bot.config_dict["autobuy"]["lifesavers"]["state"]
-                ):
-                    remaining = int(
-                        re.search(
-                            "have (.*?)x Life Saver",
-                            message.components[0].children[0].label,
-                        ).group(1)
-                    )
-                    required = int(
-                        self.bot.config_dict["autobuy"]["lifesavers"]["amount"]
-                    )
-                    if remaining < required:
-                        channel = await message.author.create_dm()
-                        await self.bot.send(
-                            "withdraw",
-                            channel,
-                            amount=str((required - remaining) * 200000),
+        if not message.guild and self.bot.state:
+            for embed in message.embeds:
+                embed = embed.to_dict()
+                # Buy lifesavers
+                try:
+                    if (
+                        embed["title"] == "Your lifesaver protected you!"
+                        and self.bot.config_dict["autobuy"]["lifesavers"]["state"]
+                    ):
+                        remaining = int(
+                            re.search(
+                                "have (.*?)x Life Saver",
+                                message.components[0].children[0].label,
+                            ).group(1)
                         )
-                        await self.bot.sub_send(
-                            "shop",
-                            "buy",
-                            channel,
-                            item="Life Saver",
-                            quantity=str(required - remaining),
+                        required = int(
+                            self.bot.config_dict["autobuy"]["lifesavers"]["amount"]
                         )
-                        return
-            except KeyError:
-                pass
+                        if remaining < required:
+                            channel = await message.author.create_dm()
+                            await self.bot.send(
+                                "withdraw",
+                                channel,
+                                amount=str((required - remaining) * 200000),
+                            )
+                            await self.bot.sub_send(
+                                "shop",
+                                "buy",
+                                channel,
+                                item="Life Saver",
+                                quantity=str(required - remaining),
+                            )
+                            return
+                except KeyError:
+                    pass
 
-            # Confirm purchase
-            try:
-                if (
-                    embed["title"] == "Pending Confirmation"
-                    and self.bot.config_dict["autobuy"]["lifesavers"]["state"]
-                ):
-                    await self.bot.click(message, 0, 1)
-            except KeyError:
-                pass
+                # Confirm purchase
+                try:
+                    if (
+                        embed["title"] == "Pending Confirmation"
+                        and self.bot.config_dict["autobuy"]["lifesavers"]["state"]
+                    ):
+                        await self.bot.click(message, 0, 1)
+                except KeyError:
+                    pass
 
         if message.channel.id != self.bot.channel_id or not self.bot.state:
             return
