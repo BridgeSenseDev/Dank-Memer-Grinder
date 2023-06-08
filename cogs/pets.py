8323f93294e16d0c8ba324f3d12cb5c5ac3c3ea5
BridgeSense <bridgesensemc@gmail.com>
2023-06-08 23:03:07 +0800

    fix: commands never resuming if pets care doesn't work

--- cogs/pets.py
+++ cogs/pets.py
@@ -21,35 +21,38 @@ class Pets(commands.Cog):
             k: v + 100 if v != 0 else float("inf") for k, v in self.bot.last_ran.items()
         }
 
-        for pet in message.components[0].children[0].options:
-            await self.bot.send("withdraw", amount="40k")
-            await message.components[0].children[0].choose(pet)
-            await asyncio.sleep(1)
-            if message.components[1].children[0].disabled:
-                continue
-            embed = message.embeds[0].to_dict()
-            for count, i in enumerate(embed["fields"]):
-                percentage = 100
-                match = re.search(r"\((\d{1,3})%\)", i["value"])
-                if match:
-                    percentage = int(match.group(1))
-                while percentage < 90:
-                    await self.bot.click(message, 1, count)
-                    if count == 2:
-                        await asyncio.sleep(10)
-                        await self.bot.click(message, 2, 1)
-                        await asyncio.sleep(0.5)
-                        await self.bot.click(message, 2, 2)
-                        break
-                    await asyncio.sleep(0.5)
-                    embed = message.embeds[0].to_dict()
-                    match = re.search(
-                        r"\((\d{1,3})%\)", embed["fields"][count]["value"]
-                    )
+        try:
+            for pet in message.components[0].children[0].options:
+                await self.bot.send("withdraw", amount="40k")
+                await message.components[0].children[0].choose(pet)
+                await asyncio.sleep(1)
+                if message.components[1].children[0].disabled:
+                    continue
+                embed = message.embeds[0].to_dict()
+                for count, i in enumerate(embed["fields"]):
+                    percentage = 100
+                    match = re.search(r"\((\d{1,3})%\)", i["value"])
                     if match:
                         percentage = int(match.group(1))
-                    else:
-                        percentage = 100
+                    while percentage < 90:
+                        await self.bot.click(message, 1, count)
+                        if count == 2:
+                            await asyncio.sleep(10)
+                            await self.bot.click(message, 2, 1)
+                            await asyncio.sleep(0.5)
+                            await self.bot.click(message, 2, 2)
+                            break
+                        await asyncio.sleep(0.5)
+                        embed = message.embeds[0].to_dict()
+                        match = re.search(
+                            r"\((\d{1,3})%\)", embed["fields"][count]["value"]
+                        )
+                        if match:
+                            percentage = int(match.group(1))
+                        else:
+                            percentage = 100
+        except AttributeError:
+            pass
 
         self.bot.last_ran = {
             k: v - 100 if v != float("inf") else 0 for k, v in self.bot.last_ran.items()
