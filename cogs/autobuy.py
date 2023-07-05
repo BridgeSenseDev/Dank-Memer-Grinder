7a1a9423e2d2064c2cacd87d85bff656a94a989b
BridgeSense <bridgesensemc@gmail.com>
2023-07-05 23:40:54 +0800

    feat: update autobuy for new dank memer shop

--- cogs/autobuy.py
+++ cogs/autobuy.py
@@ -1,3 +1,4 @@
+import asyncio
 import re
 
 from discord.ext import commands
@@ -6,6 +7,30 @@ from discord.ext import commands
 class Autobuy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.shop_message = None
+
+    async def shop_buy(self, item, count=1):
+        while not self.shop_message:
+            await self.bot.sub_send("shop", "view")
+            await asyncio.sleep(1)
+
+        found = False
+        while not found:
+            for row in range(1, 3):
+                for col, button in enumerate(
+                    self.shop_message.components[row].children
+                ):
+                    if item in button.label.lower():
+                        await self.bot.click(self.shop_message, row, col)
+
+                        modal = await self.bot.wait_for("modal")
+                        modal.components[0].children[0].answer(str(count))
+
+                        await modal.submit()
+                        return
+
+            await self.bot.click(self.shop_message, 3, 2)
+            await asyncio.sleep(0.3)
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -34,13 +59,7 @@ class Autobuy(commands.Cog):
                                 channel,
                                 amount=str((required - remaining) * 200000),
                             )
-                            await self.bot.sub_send(
-                                "shop",
-                                "buy",
-                                channel,
-                                item="Life Saver",
-                                quantity=str(required - remaining),
-                            )
+                            await self.shop_buy("saver", required - remaining)
                             self.bot.log(
                                 f"Bought {required - remaining} Lifesavers",
                                 "yellow",
@@ -62,6 +81,10 @@ class Autobuy(commands.Cog):
         if message.channel.id != self.bot.channel_id or not self.bot.state:
             return
 
+        if message.interaction and message.interaction.name == "shop view":
+            print("new message")
+            self.shop_message = message
+
         for embed in message.embeds:
             embed = embed.to_dict()
             # Shovel
@@ -72,7 +95,7 @@ class Autobuy(commands.Cog):
                     and self.bot.config_dict["autobuy"]["shovel"]
                 ):
                     await self.bot.send("withdraw", amount="35k")
-                    await self.bot.sub_send("shop", "buy", item="Shovel", quantity="1")
+                    await self.shop_buy("shovel", 1)
                     self.bot.log(
                         f"Bought Shovel",
                         "yellow",
@@ -88,9 +111,7 @@ class Autobuy(commands.Cog):
                     and self.bot.config_dict["autobuy"]["fishing"]
                 ):
                     await self.bot.send("withdraw", amount="35k")
-                    await self.bot.sub_send(
-                        "shop", "buy", item="Fishing Pole", quantity="1"
-                    )
+                    await self.shop_buy("pole", 1)
                     self.bot.log(
                         f"Bought Shovel",
                         "yellow",
@@ -106,9 +127,7 @@ class Autobuy(commands.Cog):
                     and self.bot.config_dict["autobuy"]["rifle"]
                 ):
                     await self.bot.send("withdraw", amount="35k")
-                    await self.bot.sub_send(
-                        "shop", "buy", item="Hunting Rifle", quantity="1"
-                    )
+                    await self.shop_buy("rifle", 1)
                     self.bot.log(
                         f"Bought Shovel",
                         "yellow",
