9c29b4109bc25cd8d7b7fb066cc90f36217f6c58
BridgeSense <bridgesensemc@gmail.com>
2024-09-26 18:23:23 +0800

    feat: improve blackjack formula

--- instance/blackjack.go
+++ instance/blackjack.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 )
 
+var wins, losses int
+
 func extractCardValues(emojiString string) []string {
 	re := regexp.MustCompile(`<:bjFace([0-9JQKA]+)[RB]:[0-9]+>`)
 	matches := re.FindAllStringSubmatch(emojiString, -1)
@@ -21,13 +23,6 @@ func extractCardValues(emojiString string) []string {
 	}
 
 	return cardValues
-}
-
-func bjFormula(dealer, player []string) int {
-	dealerSum, _ := calculateSum(dealer)
-	playerSum, isSoft := calculateSum(player)
-
-	return bjNewStrategy(playerSum, dealerSum, len(player), isSoft)
 }
 
 func calculateSum(cards []string) (int, bool) {
@@ -48,36 +43,295 @@ func calculateSum(cards []string) (int, bool) {
 
 	if hasAce && sum > 21 {
 		sum -= 10
+		hasAce = false
 	}
 	return sum, hasAce
 }
 
-func bjNewStrategy(playerSum, dealerSum, playerHand int, isSoft bool) int {
-	if isSoft {
-		switch {
-		case playerSum <= 17:
-			return 0 // Hit on soft 17 or lower
-		case playerSum == 18 && dealerSum >= 9:
-			return 0 // Hit on soft 18 against 9, T, A
-		case playerSum >= 19:
-			return 1 // Stand on soft 19 or higher
-		default:
-			return 1 // Stand on soft 18 against 2 through 8
-		}
+func bjFormula(dealer, player []string, canSurrender, canDoubleDown bool) int {
+	playerSum, isSoft := calculateSum(player)
+
+	return bjNewStrategy(player, dealer[0], playerSum, len(player), isSoft, canSurrender, canDoubleDown)
+}
+
+func bjNewStrategy(playerCards []string, dealerCard string, playerSum, cardCount int, isSoft, canSurrender, canDoubleDown bool) int {
+	if len(playerCards) == 2 && playerCards[0] == playerCards[1] {
+		return handlePairs(playerCards[0], dealerCard, canDoubleDown)
 	}
 
-	switch {
-	case playerSum <= 11:
-		return 0 // Hit on 11 or lower
-	case playerSum == 12 && dealerSum >= 4 && dealerSum <= 6:
-		return 1 // Stand on 12 against 4-6
-	case playerSum == 12:
-		return 0 // Hit on 12 against 2, 3, or 7+
-	case playerSum <= 16 && dealerSum >= 7:
-		return 0 // Hit against dealer 7+ for hard 13-16
-	default:
-		return 1 // Stand on 17 or higher
+	if isSoft {
+		return handleSoftHands(playerSum, dealerCard, cardCount, canDoubleDown)
 	}
+
+	return handleHardHands(playerSum, dealerCard, cardCount, canSurrender, canDoubleDown)
+}
+
+func handlePairs(playerCard, dealerCard string, canDoubleDown bool) int {
+	switch playerCard {
+	case "A", "8":
+		// P
+		return 3
+	case "10":
+		// S
+		return 1
+	case "9":
+		// S
+		if dealerCard == "7" || dealerCard == "10" || dealerCard == "A" {
+			return 1
+		}
+		// P
+		return 3
+	case "7":
+		// P
+		if dealerCard >= "2" && dealerCard <= "7" {
+			return 3
+		}
+		// H
+		return 0
+	case "6":
+		// P
+		if dealerCard >= "2" && dealerCard <= "6" {
+			return 3
+		}
+		// H
+		return 0
+	case "5":
+		// D
+		if dealerCard >= "2" && dealerCard <= "9" && canDoubleDown {
+			return 2
+		}
+		// H
+		return 0
+	case "4":
+		// P
+		if dealerCard == "5" || dealerCard == "6" {
+			return 3
+		}
+		// H
+		return 0
+	case "2", "3":
+		// P
+		if dealerCard >= "2" && dealerCard <= "7" {
+			return 3
+		}
+		// H
+		return 0
+	}
+	return 0
+}
+
+func convertFaceCard(card string) string {
+	switch card {
+	case "K", "Q", "J":
+		return "10"
+	default:
+		return card
+	}
+}
+
+func handleSoftHands(playerSum int, dealerCard string, cardCount int, canDoubleDown bool) int {
+	dealerCard = convertFaceCard(dealerCard)
+
+	switch playerSum {
+	case 20, 21:
+		// S5
+		if cardCount > 5 {
+			return 0
+		}
+		return 1
+	case 19:
+		// S4
+		if dealerCard == "10" {
+			if cardCount > 4 {
+				return 0
+			}
+			return 1
+		}
+		// S5
+		if cardCount > 5 {
+			return 0
+		}
+		return 1
+	case 18:
+		// S4
+		if dealerCard == "2" || dealerCard == "8" {
+			if cardCount > 4 {
+				return 0
+			}
+			return 1
+		}
+		// DS4
+		if dealerCard >= "3" && dealerCard <= "6" {
+			if canDoubleDown {
+				return 2
+			}
+			if cardCount > 4 {
+				return 0
+			}
+			return 1
+		}
+		// S5
+		if dealerCard == "7" {
+			if cardCount > 5 {
+				return 0
+			}
+			return 1
+		}
+		// H
+		return 0
+	case 17:
+		// D
+		if dealerCard >= "3" && dealerCard <= "6" && canDoubleDown {
+			return 2
+		}
+		// H
+		return 0
+	case 16, 15:
+		// D
+		if dealerCard >= "4" && dealerCard <= "6" && canDoubleDown {
+			return 2
+		}
+		// H
+		return 0
+	}
+	// D
+	if dealerCard == "5" || dealerCard == "6" {
+		return 2
+	}
+	// H
+	return 0
+}
+
+func handleHardHands(playerSum int, dealerCard string, cardCount int, canSurrender, canDoubleDown bool) int {
+	dealerCard = convertFaceCard(dealerCard)
+
+	switch {
+	case playerSum >= 18:
+		// S
+		return 1
+	case playerSum == 17:
+		// S5
+		if dealerCard >= "9" && cardCount > 5 {
+			return 0
+		}
+		// S
+		return 1
+	case playerSum == 16:
+		// S5
+		if dealerCard == "2" || dealerCard == "3" {
+			if cardCount > 5 {
+				return 0
+			}
+			return 1
+		}
+		// S
+		if dealerCard >= "4" && dealerCard <= "6" {
+			return 1
+		}
+		// H
+		if dealerCard == "7" || dealerCard == "8" {
+			return 0
+		}
+		// R
+		if dealerCard == "9" || dealerCard == "A" {
+			if canSurrender {
+				return 4
+			}
+			return 0
+		}
+		// Rs
+		if dealerCard == "10" {
+			if canSurrender {
+				return 4
+			}
+			return 1
+		}
+	case playerSum == 15:
+		// S5
+		if dealerCard >= "2" && dealerCard <= "6" {
+			if cardCount > 5 {
+				return 0
+			}
+			return 1
+		}
+		// H
+		if (dealerCard >= "7" && dealerCard <= "9") || dealerCard == "A" {
+			return 0
+		}
+		// R
+		if dealerCard == "10" {
+			if canSurrender {
+				return 4
+			}
+			return 0
+		}
+	case playerSum == 14:
+		// S5
+		if dealerCard >= "2" && dealerCard <= "6" {
+			if cardCount > 5 {
+				return 0
+			}
+			return 1
+		}
+		// H
+		return 0
+	case playerSum == 13:
+		// S4
+		if dealerCard == "2" || dealerCard == "3" {
+			if cardCount > 4 {
+				return 0
+			}
+			return 1
+		}
+		// S5
+		if dealerCard >= "4" && dealerCard <= "6" {
+			if cardCount > 4 {
+				return 0
+			}
+			return 1
+		}
+		// H
+		return 0
+	case playerSum == 12:
+		// H
+		if dealerCard == "2" || dealerCard == "3" {
+			return 0
+		}
+		// S4
+		if dealerCard >= "4" && dealerCard <= "6" {
+			if cardCount > 4 {
+				return 0
+			}
+			return 1
+		}
+		// H
+		return 0
+	case playerSum == 11:
+		// D
+		if dealerCard != "A" && canDoubleDown {
+			return 2
+		}
+		// H
+		return 0
+	case playerSum == 10:
+		// D
+		if dealerCard != "A" && dealerCard != "10" && canDoubleDown {
+			return 2
+		}
+		// H
+		return 0
+	case playerSum == 9:
+		// D
+		if dealerCard >= "3" && dealerCard <= "6" && canDoubleDown {
+			return 2
+		}
+		// H
+		return 0
+	default:
+		// H
+		return 0
+	}
+	return 0
 }
 
 func (in *Instance) handleButtonClick(message gateway.EventMessage, result int) {
@@ -88,6 +342,10 @@ func (in *Instance) handleButtonClick(message gateway.EventMessage, result int) 
 	case 1:
 		err = in.ClickButton(message, 0, 1)
 	case 2:
+		err = in.ClickButton(message, 0, 2)
+	case 3:
+		err = in.ClickButton(message, 0, 3)
+	case 4:
 		err = in.ClickButton(message, 1, 0)
 	}
 
@@ -97,24 +355,91 @@ func (in *Instance) handleButtonClick(message gateway.EventMessage, result int) 
 }
 
 func (in *Instance) BlackjackMessageCreate(message gateway.EventMessage) {
-	embed := message.Embeds[0]
-	if strings.Contains(embed.Author.Name, "Blackjack Game") {
-		result := bjFormula(extractCardValues(embed.Fields[0].Value), extractCardValues(embed.Fields[1].Value))
-		in.handleButtonClick(message, result)
-	}
+	in.handleBlackjackMessage(message)
 }
 
 func (in *Instance) BlackjackMessageUpdate(message gateway.EventMessage) {
-	embed := message.Embeds[0]
-	if strings.Contains(embed.Author.Name, "Blackjack Game") &&
-		!strings.Contains(message.Components[0].(*types.ActionsRow).Components[0].(*types.Button).Label, "Play Again") {
+	in.handleBlackjackMessage(message)
 
-		result := bjFormula(extractCardValues(embed.Fields[0].Value), extractCardValues(embed.Fields[1].Value))
-		in.handleButtonClick(message, result)
+	embed := message.Embeds[0]
+	var result string
+	switch embed.Color {
+	case 15022389:
+		result = "Lost"
+		losses++
+	case 16447559:
+		result = "Tied"
+	case 5025616:
+		result = "Won"
+		wins++
+	default:
+		result = ""
 	}
 
-	if netValue, err := extractNetValue(embed.Description); err == nil {
-		in.Log("others", "INF", fmt.Sprintf("%s blackjack minigame with net value: %d", netResultMessage(netValue), netValue))
+	if result != "" {
+		netValue, err := extractNetValue(embed.Description)
+		if err != nil {
+			in.Log("others", "ERR", fmt.Sprintf("Failed to extract net value: %s", err.Error()))
+			return
+		}
+
+		totalGames := wins + losses
+		winPercentage := 0.0
+		if totalGames > 0 {
+			winPercentage = (float64(wins) / float64(totalGames)) * 100
+		}
+
+		in.Log("others", "INF", fmt.Sprintf("%s blackjack minigame with net value: %d, Win %%: %.1f", result, netValue, winPercentage))
+	}
+}
+
+func (in *Instance) handleBlackjackMessage(message gateway.EventMessage) {
+	embed := message.Embeds[0]
+
+	if strings.Contains(embed.Author.Name, "Blackjack Game") && embed.Color == 2829617 {
+		var playerHandField types.EmbedField
+		for _, field := range embed.Fields {
+			if strings.Contains(field.Name, "(Player)") {
+				playerHandField = field
+				break
+			}
+		}
+
+		if playerHandField.Name == "" {
+			return
+		}
+
+		dealerCards := extractCardValues(embed.Fields[0].Value)
+		playerCards := extractCardValues(playerHandField.Value)
+
+		canSurrender := false
+		canDoubleDown := false
+
+		if len(message.Components) > 1 {
+			if surrenderRow, ok := message.Components[1].(*types.ActionsRow); ok {
+				if len(surrenderRow.Components) > 0 {
+					var surrenderButton *types.Button
+					if surrenderButton, ok = surrenderRow.Components[0].(*types.Button); ok {
+						canSurrender = !surrenderButton.Disabled
+					}
+				}
+			}
+		}
+
+		if len(message.Components) > 0 {
+			if actionRow, ok := message.Components[0].(*types.ActionsRow); ok {
+				if len(actionRow.Components) > 2 {
+					var doubleButton *types.Button
+					if doubleButton, ok = actionRow.Components[2].(*types.Button); ok {
+						canDoubleDown = !doubleButton.Disabled
+					}
+				}
+			}
+		}
+
+		result := bjFormula(dealerCards, playerCards, canSurrender, canDoubleDown)
+
+		in.handleButtonClick(message, result)
 	}
 }
 
@@ -125,15 +450,4 @@ func extractNetValue(description string) (int, error) {
 		return strconv.Atoi(strings.ReplaceAll(matches[1], ",", ""))
 	}
 	return 0, fmt.Errorf("no match found")
-}
-
-func netResultMessage(netValue int) string {
-	switch {
-	case netValue < 0:
-		return "Lost"
-	case netValue == 0:
-		return "Drawn"
-	default:
-		return "Won"
-	}
 }
